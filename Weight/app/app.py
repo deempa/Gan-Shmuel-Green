9d2227813bbb8d481a75e1d199a6af2a2da9a620
Vlad <xplodkain1@live.com>
2023-03-20 16:32:53 +0200

    added db-connector

--- Weight/app/app.py
+++ Weight/app/app.py
@@ -1,14 +1,26 @@
 from flask import Flask, request, make_response, render_template
+from db_con import showtables
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
+    
+    
     pass
 
 @app.route("/weight", methods=["GET","POST"])
 def weight():
     return render_template("index.html")
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
