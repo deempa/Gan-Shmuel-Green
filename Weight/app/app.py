2146297826874f85d9365f80c7ed27868e133cc4
Vlad <xplodkain1@live.com>
2023-03-20 21:47:14 +0200

    added handlers for the buttons

--- Weight/app/app.py
+++ Weight/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response, render_template, redirect, url_for
 import csv
 import os
-from datetime import datetime,date
+from datetime import datetime, date
 import mysql.connector
 from werkzeug.utils import secure_filename
 
@@ -24,55 +24,64 @@ isconnected = 0
 
 app = Flask(__name__)
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def root():
     pass
 
-@app.route("/weight", methods=["GET","POST"])
+
+@app.route("/weight", methods=["GET", "POST"])
 def post_weight():
     if request.method == 'POST':
-        direction = request.form['direction']
-        truck_license = request.form['truck_license']
-        product_delivered = request.form['product_delivered']
-        truck_bruto_weight = request.form['truck_bruto_weight']
-        unit_of_measure_bruto = request.form['unit_of_measure_1']
-        truck_neto_weight = request.form['truck_neto_weight']
-        unit_of_measure_neto = request.form['unit_of_measure_2']
-        timestamp = datetime.now().strftime(r"%Y%m%d%H%M%S")
+        if request.form['action'] == 'submit':
+
+            direction = request.form['direction']
+            truck_license = request.form['truck_license']
+            product_delivered = request.form['product_delivered']
+            truck_bruto_weight = request.form['truck_bruto_weight']
+            unit_of_measure_bruto = request.form['unit_of_measure_1']
+            truck_neto_weight = request.form['truck_neto_weight']
+            unit_of_measure_neto = request.form['unit_of_measure_2']
+            timestamp = datetime.now().strftime(r"%Y%m%d%H%M%S")
+            return (f"{direction},{truck_license},{product_delivered},{truck_bruto_weight},{unit_of_measure_bruto},{truck_neto_weight},{unit_of_measure_neto},{timestamp}")
         
-        if 'file' not in request.files:
-            return "no file part"
-        file = request.files['file']
-        if file.filename == '':
-            return "No selected file"
+        elif request.form['action'] == 'upload':
+            if 'file' not in request.files:
+                return "no file part"
+            file = request.files['file']
+            if file.filename == '':
+                return "No selected file"
 
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-            with open(f'./{filename}', 'r') as file:
-                reader = csv.reader(file)
-                for row in reader:
-                    rows = print(row)
-        
-        return redirect(url_for('post_weight'))
+                with open(f'./{filename}', 'r') as file:
+                    reader = csv.reader(file)
+                    for row in reader:
+                        rows = print(row)
+
+            return redirect(url_for('post_weight'))
     elif request.method == 'GET':
         return render_template('index.html')
-    
+
+
 @app.route("/health")
 def healthcheck():
     if isconnected == 1:
         return "The Server Is Healthy", 200
     else:
         return "Could Not Connect To The Database", 500
-        
-    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
