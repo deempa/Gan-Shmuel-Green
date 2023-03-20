33efbb293aa1834f7b35f908d1ee25cd4c294b0a
gru <gray31318@gmail.com>
2023-03-20 18:13:01 +0200

    check health draft 1

--- Billing/app.py
+++ Billing/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, make_response
 import sqlalchemy
 
-app=Flask(__name__)
+app = Flask(__name__)
 
+""" @app.route('/provider')
+def post_provider():
+    return "in progres" """
+
+@app.route('/health')
+def check_health():
+        return "OK", 200
+        
 engine = sqlalchemy.create_engine("mysql+pymysql://billdbuser:billdbpass@mysql-server/billdb")
 
 
