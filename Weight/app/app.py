8388946e9dcb4c40055eace7863d5386ad2a6b54
Ofir Aviv <ofirsecure@gmail.com>
2023-03-20 13:54:13 +0200

    added HTML with a Weight Hub

--- Weight/app/app.py
+++ Weight/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
 
@@ -8,4 +8,7 @@ def root():
 
 @app.route("/weight", methods=["GET","POST"])
 def weight():
-    pass
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
