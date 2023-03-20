117da7106d8d2ab3308dc0d5adc33e7a60ce7fad
Avihai Ziv <avihai40@gmail.com>
2023-03-20 15:57:11 +0200

    added sql connection string

--- Billing/app.py
+++ Billing/app.py
@@ -1,1 +1,10 @@
 from flask import Flask
+import sqlalchemy
+
+app=Flask(__name__)
+
+engine = sqlalchemy.create_engine("mysql+pymysql://billdbuser:billdbpass@mysql-server/billdb")
+
+
+if __name__ == "__main__":
+    app.run()
