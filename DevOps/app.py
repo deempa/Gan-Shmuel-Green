724726d5c4be80c59b59a82b6f3507ee34136451
Lior Rabanian <masrab11@gmail.com>
2023-03-20 17:08:27 +0200

    changed file

--- DevOps/app.py
+++ DevOps/app.py
@@ -11,18 +11,11 @@ def payload():
             data = request.get_json()
             branch_name = data['ref'].split('/')[-1]
             repo_url = data['repository']['clone_url']
-            repo_name = data['repository']['name']
-            
-            # Clone and checkout to the branch that push
-            os.mkdir("./Cloned")
-            os.chdir("./Cloned")       
-            print(os.getcwd())
-            # subprocess.run(['git', 'clone', repo_url])
-            # os.chdir("./" + repo_name)
-            # subprocess.run(['git', 'checkout', branch_name])
-            
+            repo_name = data['repository']['name']     
+            subprocess.run(['git', 'clone', repo_url])
+            os.chdir("./" + repo_name)
+            subprocess.run(['git', 'checkout', branch_name])        
             return f"{branch_name}, {repo_url}, {repo_name}"
             
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8081)
