c280a8f9d9f1a7e24690a6db3e68c0983fede5bd
Lior Rabanian <masrab11@gmail.com>
2023-03-20 21:40:54 +0200

    changed

--- DevOps/app.py
+++ DevOps/app.py
@@ -44,10 +44,10 @@ def clone(repo_url, repo_name):
     Repo.clone_from(repo_url, f"./{repo_name.lower()}")
         
 def build(repo_name, branch_name):
-    client.images.build(path=f"./{repo_name.lower()}/{branch_name}/")
+    client.images.build(path=f"./{repo_name.lower()}/{branch_name.lower()}/")
     
 def run(branch_name):
-    client.containers.run(f"{branch_name}_image", detach=True, hostname=f"{branch_name}_app", ports={'8082': '5000'})
+    client.containers.run(f"{branch_name.lower()}_image", detach=True, hostname=f"{branch_name.lower()}_app", ports={'8082': '5000'})
 
         
 @app.route("/health", methods=["GET"])
