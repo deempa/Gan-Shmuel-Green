115190e61b9c07bfb8d4c57a1f56fa97cfe88974
Lior Rabanian <masrab11@gmail.com>
2023-03-21 11:11:45 +0200

    testing

--- DevOps/app.py
+++ DevOps/app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 client = docker.from_env()
 
-
 @app.route("/trigger", methods=["GET", "POST"])
 def trigger():
     if request.method == "POST":
@@ -17,39 +16,23 @@ def trigger():
             repo_url = data['repository']['clone_url']
             repo_name = data['repository']['name']    
             pusher = data['pusher']['name']
-            
+            committer_email = data['commits'][0]['committer']['email']
+              
+            # Delete Cloned Repo If Exists.
             try:
                 os.system(f"rm -rf ./{repo_name}")
             except:
                 pass
             
-            print("Cloning....")
-            clone(repo_url, repo_name)
-            print("Finished Cloning")
-            
-            
-            print("Building....")
-            # build(repo_name, branch_name)
-            build(repo_name, "Billing")
-            # build(repo_name, "Weight")
-            # build(repo_name, "Billing")
-            print("Finished Building")
-            # tests
-            
+            clone(repo_url, repo_name)                    
+            build(repo_name, branch_name)           
+            # tests          
             print("testing..... Completed")
             
-            print("Running...")
+            # print("Running...")
             # run(branch_name)
-            run("Billing")
-            # run("Weight")
-            print("Finished Running")
-            # if branch_name == "main":
-            #     Repo.clone_from(repo_url, "./Gan-Shmuel-Green")
-            #     client.images.build(path="./Gan-Shmuel-Green/Billing/")
-            #     client.containers.run("billing_image", detach=True, hostname="billing_app", ports={'8082': '5000'})
-                
-                      
-            return f"{branch_name}, {repo_url}, {repo_name}"
+              
+            return "OK", 200
         
 def clone(repo_url, repo_name):
     Repo.clone_from(repo_url, f"./{repo_name}")
@@ -60,7 +43,10 @@ def build(repo_name, branch_name):
     
 def run(branch_name):
     lower_branch_name = branch_name.lower()
-    client.containers.run(f"{lower_branch_name}_image", detach=True, hostname=f"{branch_name}_app", ports={'8082': '5000'})
+    client.containers.run(f"{lower_branch_name}_image", detach=True, hostname=f"{branch_name}_app", ports={'8082': '5000'}, entrypoint="python3 app.py")
+ 
+def mailing_Feature():
+    pass
 
         
 @app.route("/health", methods=["GET"])
