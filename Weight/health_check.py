2f92e9a182c6e6992b3ce04c700eea2b578d6e19
Hadar David <hadardavid0011@gmail.com>
2023-03-22 12:07:12 +0200

    removde some

--- Weight/health_check.py
+++ Weight/health_check.py
@@ -1,16 +1,27 @@
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+health_status = True
 
-@app.route('/')
-def home():
-    if isconnected == 1:
-        return ("The Server Is Healthpy, status 200")
-    elif isconnected == 0:
-        return ("The Server Is Not Healthpy, status 500")
+@app.route('/toggle')
+def toggle():
+    global health_status
+    health_status = not health_status
+    return jsonify(health_value=health_status)
 
 
-if __name__ == "__main__":
+@app.route('/health')
+def health():
+    if health_status:
+        resp = jsonify(health="healthy")
+        resp.status_code = 200
+    else:
+        resp = jsonify(health="unhealthy")
+        resp.status_code = 500
+
+    return resp
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
