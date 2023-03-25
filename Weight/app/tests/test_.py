cafac2663128b5e991dfdaa0bd0c8d261344010a
Vlad <xplodkain1@live.com>
2023-03-25 23:07:20 +0300

    minor fixes,more tests

--- Weight/app/tests/test_.py
+++ Weight/app/tests/test_.py
@@ -6,8 +6,8 @@ import json
 
 import requests
 
-# ENDPOINT="https://3.76.109.165:8089"
-ENDPOINT="http://127.0.0.1:5000"
+ENDPOINT="https://3.76.109.165:8089"
+# ENDPOINT="http://127.0.0.1:5000"
 
 ## HEALTH TEST
 def test_health():
@@ -17,13 +17,13 @@ def test_health():
 
 
 
-def test_api_endpoint():
+def test_weight():
     response = requests.get(ENDPOINT +'/weight')
     assert response.status_code == 200
-    
+def test_batch_weight():    
     response = requests.get(ENDPOINT +'/batch-weight')
     assert response.status_code == 200
-    
+def test_unknown():    
     response = requests.get(ENDPOINT +'/unknown')
     assert response.status_code == 200
     
@@ -52,45 +52,52 @@ def test_post_bad_request():
     response = requests.post(ENDPOINT +'/weight', data=payload)
     assert response.status_code == 400
 #Container id is required
+def test_empty_contaier():
     payload = {'direction': 'in', 'truck_license': 'lic10', 'product_delivered': 'apples', 
             'truck_bruto_weight': '12000', 'unit_of_measure_1': 'kg', 'force': 'True',
               'container_id': ''}
     response = requests.post(ENDPOINT +'/weight', data=payload)
     assert response.status_code == 400
     
+
+def test_invalid_product():
      #invalid product 
     payload = {'direction': 'in', 'truck_license': 'lic10', 'product_delivered': '123', 
             'truck_bruto_weight': '12000', 'unit_of_measure_1': 'kg', 'force': 'True',
               'container_id': 'T-123'}
     response = requests.post(ENDPOINT +'/weight', data=payload)
     assert response.status_code == 400
-
+def test_invalid_weight():
 #invalid weight
     payload = {'direction': 'in', 'truck_license': 'lic10', 'product_delivered': 'apples', 
             'truck_bruto_weight': 'asd', 'unit_of_measure_1': 'kg', 'force': 'True',
               'container_id': 'T-123'}
     response = requests.post(ENDPOINT +'/weight', data=payload)
     assert response.status_code == 400
-#empty weight
+def test_empty_weight():
     payload = {'direction': 'in', 'truck_license': 'lic10', 'product_delivered': 'apples', 
             'truck_bruto_weight': '', 'unit_of_measure_1': 'kg', 'force': 'True',
               'container_id': 'T-123'}
     response = requests.post(ENDPOINT +'/weight', data=payload)
     assert response.status_code == 400
+def test_out_before_in():
+    payload = {'direction': 'out', 'truck_license': '131313131', 'product_delivered': 'apples', 
+            'truck_bruto_weight': '12000', 'unit_of_measure_1': 'kg', 'force': 'True',
+              'container_id': '123123123'}
+    response = requests.post(ENDPOINT +'/weight', data=payload)
+    assert response.status_code == 400
 
-    # payload = {'direction': 'out', 'truck_license': '131313131', 'product_delivered': 'apples', 
-    #         'truck_bruto_weight': '12000', 'unit_of_measure_1': 'kg', 'force': 'True',
-    #           'container_id': '123123123'}
-    # response = requests.post(ENDPOINT +'/weight', data=payload)
-    # assert response.status_code == 400
 
 
-
-def get_session():
+def test_get_session():
     response = requests.get(ENDPOINT +'/session/1')
-    assert response.status_code == 200 or response.status_code==400
+    assert response.status_code == 200 or response.status_code==404
 
 
-def get_item():
+def test_get_item():
     response= requests.get(ENDPOINT + '/item/1')
-    assert response.status_code == 200 or response.status_code==400
+    assert response.status_code == 200 or response.status_code==404
+
+def test_main():
+    response= requests.get(ENDPOINT + '/')
+    assert response.status_code == 200 or response.status_code==404
