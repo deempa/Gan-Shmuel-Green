7db9c8f6da29ea3179c70a12ecafbcb4b0490b92
Avihai Ziv <avihai40@gmail.com>
2023-03-26 10:53:47 +0300

    fixed bill

--- Billing/app.py
+++ Billing/app.py
@@ -272,7 +272,6 @@ def js_truck_session_count(truck_ids,t1,t2):
     return totalsessions
 
 def js_prod_sess(product_id,truck_ids,t1,t2):
-    ##working
     sumkg=0
     sessioncount=0
     sessions=[]
